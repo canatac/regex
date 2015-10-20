8bf139f8002a569464e3ffc64f3458d50dd7d85e
Can ATAC <can.atac@gmail.com>
2015-10-20 14:36:33 +0200

    Initial Commit

---
+++ regex/regex.h
@@ -0,0 +1,19 @@
+//
+//  regex.h
+//  regex
+//
+//  Created by Can ATAC on 20/10/2015.
+//  Copyright © 2015 Can ATAC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for regex.
+FOUNDATION_EXPORT double regexVersionNumber;
+
+//! Project version string for regex.
+FOUNDATION_EXPORT const unsigned char regexVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <regex/PublicHeader.h>
+
+
